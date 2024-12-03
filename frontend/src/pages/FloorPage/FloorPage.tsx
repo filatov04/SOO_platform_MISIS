0ef7758f88442c4376fe00813e29afb6b8db5416
filatov04 <filka04012004@yandex.ru>
2024-12-03 20:30:05 +0300

    uninstall react-calendar

--- frontend/src/pages/FloorPage/FloorPage.tsx
+++ frontend/src/pages/FloorPage/FloorPage.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './FloorPage.scss';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import tg from '../../shared/assets/FloorPage/ContactInfo/Telegram.png';
-import phone from '../../shared/assets/FloorPage/ContactInfo/phone.png';
-import { useNavigate } from 'react-router-dom';
-import { document_type, RoomFloor } from '../../features/RoomFloor/RoomFloor';
-import { useAppSelector } from '../../app/hooks/hooks';
-import { headmansInfo } from '../../app/features/Headmans/HeadmansSlice';
-import { ModalCreateViolation } from '../../features/ModalCreateViolation';
-import axios from 'axios';
+import React, { useEffect, useRef, useState } from "react";
+import "./FloorPage.scss";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import tg from "../../shared/assets/FloorPage/ContactInfo/Telegram.png";
+import phone from "../../shared/assets/FloorPage/ContactInfo/phone.png";
+import { useNavigate } from "react-router-dom";
+import { RoomFloor } from "../../features/RoomFloor/RoomFloor";
+import { useAppSelector } from "../../app/hooks/hooks";
+import { headmansInfo } from "../../app/features/Headmans/HeadmansSlice";
+import { ModalCreateViolation } from "../../features/ModalCreateViolation";
+import axios from "axios";
 
 interface roomData {
   document_type: string;
@@ -46,14 +46,16 @@ interface blockViolation {
 }
 
 export const FloorPage = () => {
-  const floor = localStorage.getItem('NumberFloor');
+  const floor = localStorage.getItem("NumberFloor");
   const router = useNavigate();
   const headmans = useAppSelector(headmansInfo);
-  const floorId = localStorage.getItem('FloorId');
+  const floorId = localStorage.getItem("FloorId");
   const modalRef = useRef(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [roomViolation, setRoomViolation] = useState('');
-  const [roomsWithViolation, setRoomsWithViolations] = useState<blockViolation>({});
+  const [roomViolation, setRoomViolation] = useState("");
+  const [roomsWithViolation, setRoomsWithViolations] = useState<blockViolation>(
+    {}
+  );
 
   function mergeRoomsViolations(data: roomsData[]) {
     const blockMap: blockViolation = {};
@@ -61,7 +63,10 @@ export const FloorPage = () => {
       let violation: violation[] = [];
       let roomNumber = room.room_number;
       if (room.violations.length == 0) {
-        blockMap[room.block_number] = [...(blockMap[room.block_number] || []), ...violation];
+        blockMap[room.block_number] = [
+          ...(blockMap[room.block_number] || []),
+          ...violation,
+        ];
       } else {
         for (const violations of room.violations) {
           violation.push({
@@ -71,9 +76,12 @@ export const FloorPage = () => {
             description: violations.description,
             room_id: violations.room_id,
             room_number: roomNumber,
-            witness: violations.witness
+            witness: violations.witness,
           });
-          blockMap[room.block_number] = [...(blockMap[room.block_number] || []), ...violation];
+          blockMap[room.block_number] = [
+            ...(blockMap[room.block_number] || []),
+            ...violation,
+          ];
         }
       }
     }
@@ -83,10 +91,10 @@ export const FloorPage = () => {
   useEffect(() => {
     async function getRoomWithViolation() {
       const getRoom = await axios
-        .get('http://localhost:8000/violations/rooms/get/' + floorId, {
+        .get("http://localhost:8000/violations/rooms/get/" + floorId, {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem('authToken')}`
-          }
+            Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          },
         })
         .then((response) => {
           mergeRoomsViolations(response.data);
@@ -100,33 +108,46 @@ export const FloorPage = () => {
   }, []);
 
   return (
-    <div className='floor-page'>
-      <div className='floor-page__info'>
-        <div className='floor-page__arrow-back' onClick={() => router('/MainPage')}>
-          <ArrowBackIosNewIcon sx={{ color: '#187FF6', width: '68px', height: '68px' }} />
+    <div className="floor-page">
+      <div className="floor-page__info">
+        <div
+          className="floor-page__arrow-back"
+          onClick={() => router("/MainPage")}
+        >
+          <ArrowBackIosNewIcon
+            sx={{ color: "#187FF6", width: "68px", height: "68px" }}
+          />
         </div>
-        <div className='floor-page__number'> {floor} этаж</div>
-        <div className='floor-page__contact-info'>
-          <div className='floor-page__contact-elder'>Староста</div>
-          <div className='floor-page__contact-name'>
-            {floorId && headmans[floorId]?.secondName ? headmans[floorId].secondName : 'found'}{' '}
-            {floorId && headmans[floorId]?.firstName ? headmans[floorId].firstName : 'Not'}
+        <div className="floor-page__number"> {floor} этаж</div>
+        <div className="floor-page__contact-info">
+          <div className="floor-page__contact-elder">Староста</div>
+          <div className="floor-page__contact-name">
+            {floorId && headmans[floorId]?.secondName
+              ? headmans[floorId].secondName
+              : "found"}{" "}
+            {floorId && headmans[floorId]?.firstName
+              ? headmans[floorId].firstName
+              : "Not"}
           </div>
         </div>
-        <div className='floor-page__contact-info'>
-          <div className='floor-page__contact-elder'>
-            <img src={tg} className='floor-page__tg' />
-            {floorId && headmans[floorId]?.tg ? headmans[floorId].tg : 'Not found'}
+        <div className="floor-page__contact-info">
+          <div className="floor-page__contact-elder">
+            <img src={tg} className="floor-page__tg" />
+            {floorId && headmans[floorId]?.tg
+              ? headmans[floorId].tg
+              : "Not found"}
           </div>
-          <div className='floor-page__contact-name'>
-            <img src={phone} className='floor-page__phone' />+
-            {floorId && headmans[floorId]?.phone ? headmans[floorId].phone : 'Not found'}
+          <div className="floor-page__contact-name">
+            <img src={phone} className="floor-page__phone" />+
+            {floorId && headmans[floorId]?.phone
+              ? headmans[floorId].phone
+              : "Not found"}
           </div>
         </div>
       </div>
-      <div className='floor-page__rooms'>
+      <div className="floor-page__rooms">
         {Object.entries(roomsWithViolation).map(([key, value]) => {
-          console.log('1');
+          console.log("1");
           return (
             <RoomFloor
               floor={floor}
